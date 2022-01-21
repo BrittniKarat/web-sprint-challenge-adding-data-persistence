0367cd6f15b7daf06dc828fc80576d81d8110935
BunniMK <86545296+BunniMK@users.noreply.github.com>
2022-01-20 22:00:08 -0700

    This is a long set up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+
+server.use(express.json())
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} is not a valid address`})
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({message: ` Error: ${err.message}`})
+})
+
+module.exports = server;
